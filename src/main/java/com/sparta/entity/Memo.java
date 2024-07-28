1bacb62eba3f507b81832720af5f87042c91418a
seeyoungryu <seayeux@gmail.com>
2024-07-28 16:08:54 +0900

    update - EntityTest.java, Memo.java

--- src/main/java/com/sparta/entity/Memo.java
+++ src/main/java/com/sparta/entity/Memo.java
@@ -10,8 +10,11 @@ import jakarta.persistence.Table;
 @Entity // JPA가 관리할 수 있는 Entity 클래스 지정
 @Table(name = "memo") // 매핑할 테이블의 이름을 지정
 public class Memo {
+
     @Id  //기본키* -> 엔티티 구분 (식별자) 할 수 있게 함!
     private Long id;
+//    @GeneratedValue(strategy = GenerationType.IDENTITY) //태스트
+
 
     // nullable: null 허용 여부 (기본값 true)
     // unique: 중복 허용 여부 (기본값 false -> false 일때 중복 허용)
@@ -21,4 +24,28 @@ public class Memo {
     // length: 컬럼 길이 지정
     @Column(name = "contents", nullable = false, length = 500)
     private String contents;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
 }
